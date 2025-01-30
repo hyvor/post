41fd662e4526ed58b93c656e36bc9dcc84fe0358
Nolab <thibault.boutet@epita.fr>
2025-01-30 18:23:16 -0500

    Add list view

--- frontend/src/routes/console/[id]/+page.svelte
+++ frontend/src/routes/console/[id]/+page.svelte
@@ -1,10 +1,12 @@
 <script>
 	import SingleBox from "./@components/content/SingleBox.svelte";
-	import HomeStats from "./HomeStats.svelte";
+	import HomeStats from "./@components/home/HomeStats.svelte";
+	import ListView from "./@components/home/ListView.svelte";
 
     
 </script>
 
 <SingleBox style="overflow-auto">
     <HomeStats />
+	<ListView />
 </SingleBox>
--- frontend/src/routes/console/types.ts
+++ frontend/src/routes/console/types.ts
@@ -3,3 +3,9 @@ export interface Stats {
 	issues: { total: number; last_30d: number };
 	lists: { total: number; last_30d: number };
 }
+
+export type List = {
+	id: number,
+	name: string,
+	subscribers_count: { total: number; last_30d: number }
+}
