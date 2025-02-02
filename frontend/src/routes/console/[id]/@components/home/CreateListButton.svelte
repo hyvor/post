f05b07cf5cc2570f36144d76de1b4d241124875b
Nolab <thibault.boutet@epita.fr>
2025-02-02 10:10:24 -0500

    Add modal

---
+++ frontend/src/routes/console/[id]/@components/home/CreateListButton.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import { ActionList, ActionListItem, Button, Caption, Dropdown, FormControl, IconButton, Label, Modal, Radio, TextInput, toast } from "@hyvor/design/components";
+
+    let modalOpen = false;
+	let showDropdown = false;
+
+</script>
+
+<Button
+	color="accent"
+	on:click={() => modalOpen = true}
+	size="small"
+>
+	Create List
+</Button>
+
+<Modal
+	title="Publish Post"
+	bind:show={modalOpen}
+	size="large"
+	footer={{
+        cancel: {
+            text: 'Cancel',
+        },
+        confirm: {
+            text: 'Create',
+        }
+    }}
+    on:cancel={() => {
+		modalOpen = false;
+	}}
+    on:confirm={() => {
+        modalOpen = false;
+    }}
+>
+
+	<div class="modal-inner">
+		<FormControl>
+			<Label>Name</Label>
+			<TextInput placeholder="Enter list name" />
+		</FormControl>
+		<FormControl>
+			<Label>Project</Label>
+		</FormControl>
+	</div>
+
+
+
+
+</Modal>
