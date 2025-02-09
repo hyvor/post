f625f7e2a85ef15135a976afbff16af4391292d6
Supun Wimalasena <supun@hyvor.com>
2025-02-09 03:46:22 +0100

    symfopny migrations

--- symfony/migrations/Version20250209023815.php
+++ symfony/migrations/Version20250209023815.php
@@ -7,25 +7,28 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20250209023815 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Creates the projects table';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-
+        $this->addSql(<<<SQL
+        CREATE TABLE projects (
+            id BIGSERIAL PRIMARY KEY,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+            user_id BIGINT NOT NULL,
+            name VARCHAR(255) NOT NULL
+        );
+        SQL);
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-
+        $this->addSql('DROP TABLE projects');
     }
 }
---
+++ symfony/migrations/Version20250209024410.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250209024410 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Create the subscribers table';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql(<<<SQL
+        CREATE TYPE subscriber_status AS ENUM ('subscribed', 'unsubscribed', 'pending');
+        CREATE TYPE subscriber_source AS ENUM ('console', 'form', 'import', 'auto_subscribe');
+
+        CREATE TABLE subscribers (
+            id BIGSERIAL PRIMARY KEY,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+            list_id BIGINT NOT NULL references lists(id),
+            email VARCHAR(255) NOT NULL UNIQUE,
+            status subscriber_status DEFAULT 'pending',
+            subscribed_at TIMESTAMP,
+            unsubscribed_at TIMESTAMP,
+            source subscriber_source DEFAULT 'form',
+            source_id VARCHAR(255),
+            subscribe_ip VARCHAR(255),
+            unsubscribe_reason VARCHAR(255)
+        );
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE subscribers');
+        $this->addSql('DROP TYPE subscriber_status');
+        $this->addSql('DROP TYPE subscriber_source');
+    }
+}
