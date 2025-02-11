a70507e1847efb7476d92c0aed69be58e86c9ca1
Supun Wimalasena <supun@hyvor.com>
2025-02-11 13:55:56 +0100

    wip

--- backend/config/packages/security.php
+++ backend/config/packages/security.php
@@ -1,13 +1,21 @@
 <?php
 
-// config/packages/security.php
 use Hyvor\Internal\Bundle\Security\HyvorAuthenticator;
+use Hyvor\Internal\Bundle\Security\UserRole;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Config\SecurityConfig;
 
-return static function (SecurityConfig $security): void {
-    // ....
+return static function (ContainerBuilder $container, SecurityConfig $security): void {
 
-    $security->firewall('main')
-        ->stateless(true)
-        ->customAuthenticators([HyvorAuthenticator::class]);
+//    $security
+//        ->firewall('hyvor_auth')
+//        ->stateless(true)
+//        ->lazy(true)
+//        ->customAuthenticators([HyvorAuthenticator::class]);
+//
+//    $security
+//        ->accessControl()
+//        ->path('^/api/resource')
+//        ->roles(UserRole::USER);
+
 };
