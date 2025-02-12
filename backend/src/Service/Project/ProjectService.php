64e0385bf007a35dc4348a7faab53f429219c6bb
Supun Wimalasena <supun@hyvor.com>
2025-02-12 14:22:52 +0100

    refactor again

---
+++ backend/src/Service/Project/ProjectService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Service\Project;
+
+use App\Entity\Project;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ProjectService
+{
+
+    public function __construct(
+        private EntityManagerInterface $entityManager
+    )
+    {
+    }
+
+    public function createProject(
+        string $name,
+    ): Project
+    {
+
+        $project = new Project();
+        $project->setName($name);
+        $project->setUserId(1); # TODO: Replace with actual user ID
+        $project->setCreatedAt(new \DateTimeImmutable());
+        $project->setUpdatedAt(new \DateTimeImmutable());
+
+        $this->entityManager->persist($project);
+        $this->entityManager->flush();
+
+        return $project;
+
+    }
+
+}
