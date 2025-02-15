eb4c1d36f630994a76239c0f2ca739b84211f47a
Nolab <thibault.boutet@epita.fr>
2025-02-15 13:47:23 +0100

    Add test for non empty projects

--- backend/tests/Api/Console/Project/GetProjectsTest.php
+++ backend/tests/Api/Console/Project/GetProjectsTest.php
@@ -3,6 +3,8 @@
 namespace Api\Console\Project;
 
 use App\Api\Console\Controller\ProjectController;
+use App\Entity\Factory\ProjectFactory;
+use App\Entity\Project;
 use App\Service\Project\ProjectService;
 use App\Tests\Case\WebTestCase;
 use PHPUnit\Framework\Attributes\CoversClass;
@@ -30,4 +32,22 @@ class GetProjectsTest extends WebTestCase
         $this->assertEquals(0, count($data));
     }
 
+    public function testListProjectsNonEmpty(): void
+    {
+        $projects = $this
+            ->factory(ProjectFactory::class)
+            ->createMany(10);
+
+        $response = $this->consoleApi('GET', '/projects');
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $content = $response->getContent();
+        $this->assertNotFalse($content);
+        $this->assertJson($content);
+
+        $data = json_decode($content, true);
+        $this->assertIsArray($data);
+        $this->assertEquals(10, count($data));
+    }
 }
