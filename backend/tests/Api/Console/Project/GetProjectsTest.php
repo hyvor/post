aa924502d1ea90e3dca309e36592bce891d5b44c
Nolab <thibault.boutet@epita.fr>
2025-02-15 13:54:39 +0100

    Fix tests to get project

--- backend/tests/Api/Console/Project/GetProjectsTest.php
+++ backend/tests/Api/Console/Project/GetProjectsTest.php
@@ -50,4 +50,24 @@ class GetProjectsTest extends WebTestCase
         $this->assertIsArray($data);
         $this->assertEquals(10, count($data));
     }
+
+    public function testGetSpecificProjet(): void
+    {
+        $project = $this
+            ->factory(ProjectFactory::class)
+            ->create(fn (Project $project) => $project->setName('Valid Project Name'));
+
+        $response = $this->consoleApi('GET', '/projects/' . $project->getId());
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $content = $response->getContent();
+        $this->assertNotFalse($content);
+        $this->assertJson($content);
+
+        $data = json_decode($content, true);
+        $this->assertIsArray($data);
+        $this->assertEquals($project->getId(), $data['id']);
+        $this->assertEquals($project->getName(), $data['name']);
+    }
 }
