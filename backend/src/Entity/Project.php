f8307717a87408a4c5a5b0fc5827924d930ca526
Supun Wimalasena <supun@hyvor.com>
2025-02-21 18:21:21 +0100

    symfony clock, timestamptz

--- backend/src/Entity/Project.php
+++ backend/src/Entity/Project.php
@@ -6,6 +6,7 @@ use App\Repository\ProjectRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation\Timestampable;
 use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: ProjectRepository::class)]
