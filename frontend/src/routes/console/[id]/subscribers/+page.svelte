216b7bad5665dd5beb6aa84e097694edf2728592
Nolab <thibault.boutet@epita.fr>
2025-02-26 11:02:47 +0100

    Add subscriber page

---
+++ frontend/src/routes/console/[id]/subscribers/+page.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import { Button, ButtonGroup, ActionList, ActionListItem } from '@hyvor/design/components';
+	import Selector from '../@components/content/Selector.svelte';
+	import type { NewsletterSubscriberStatus } from '../../types';
+    import IconBoxArrowInDown from '@hyvor/icons/IconBoxArrowInDown';
+    import IconPlus from '@hyvor/icons/IconPlus';
+	import SingleBox from '../@components/content/SingleBox.svelte';
+
+    let showStatus = false;
+	let showSegment = false;
+
+    let addingManually = false;
+	let importing = false;
+
+    $: statusKey = 'subscribed';
+
+    function selectStatus(s: NewsletterSubscriberStatus) {
+		showStatus = false;
+		status = s;
+	}
+
+</script>
+<SingleBox>
+    <div class="content">
+        <div class="left">
+            <Selector name="Status" bind:show={showStatus} value={statusKey} width={200}>
+                <ActionList selection="single" selectionAlign="end">
+                    <ActionListItem
+                        on:click={() => selectStatus('subscribed')}
+                        selected={status === 'subscribed'}
+                    >
+                        Subscribed
+                    </ActionListItem>
+                    <ActionListItem
+                        on:click={() => selectStatus('unsubscribed')}
+                        selected={status === 'unsubscribed'}
+                    >
+                        Unsubscribed
+                    </ActionListItem>
+                </ActionList>
+            </Selector>
+        </div>
+        <ButtonGroup>
+            <Button size="small" on:click={() => (importing = true)}>
+                <IconBoxArrowInDown slot="end" /> Import
+            </Button>
+            <Button size="small" on:click={() => (addingManually = true)}>
+                <IconPlus slot="end" /> Add Manually
+            </Button>
+        </ButtonGroup>
+    </div>
+</SingleBox>
+
+
+<style>
+	.content {
+		padding: 25px 35px;
+	}
+	.left {
+		flex: 1;
+	}
+
+	@media (max-width: 992px) {
+		.top {
+			flex-direction: column;
+		}
+		.left {
+			margin-bottom: 10px;
+		}
+	}
+</style>
