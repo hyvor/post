d59c7c698ca06a875b9987228b1dde0d8318c29d
Nolab <thibault.boutet@epita.fr>
2025-03-10 11:28:20 -0400

    Complete handler

--- backend/src/MessageHandler/SendEmailHandler.php
+++ backend/src/MessageHandler/SendEmailHandler.php
@@ -2,15 +2,34 @@
 
 namespace App\MessageHandler;
 
+use App\Entity\Issue;
+use App\Entity\Subscriber;
 use App\Message\SendEmailMessage;
+use App\Service\Issue\SendService;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
 class SendEmailHandler
 {
-    public function __invoke(SendEmailMessage $message)
+    private SendService $sendService;
+    public function __construct(
+        SendService $sendService,
+    )
+    {
+    }
+
+    private function sendEmail(Subscriber $subscriber): void
+    {
+        // TODO: send email
+    }
+
+    public function __invoke(SendEmailMessage $message): void
     {
         // TODO: Implement __invoke() method to send email
-        dd($message);
+        $this->sendService->paginateSendableSubscribers(
+            $message->getIssue(),
+            1000,
+            [$this, 'sendEmail']
+        );
     }
 }
