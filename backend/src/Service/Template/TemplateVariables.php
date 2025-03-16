00076075ac1e357b9d8c03ba2a90ee8ca3c3ef2a
Supun Wimalasena <supun@hyvor.com>
2025-03-16 03:15:38 +0100

    templates initial

--- backend/src/Service/Template/TemplateVariables.php
+++ backend/src/Service/Template/TemplateVariables.php
@@ -2,10 +2,39 @@
 
 namespace App\Service\Template;
 
+// https://post.hyvor.com/docs/email-templates
 class TemplateVariables
 {
 
     public function __construct(
+        // language code to be used in <html> tag
+        public string $lang,
+
+        // subject of the email to be used in <title> tag
+        public string $subject,
+
+        // content in HTML format
+        public string $content,
+
+        // header
+        public string $logo,
+        public string $logo_alt,
+        public string $brand,
+        public string $brand_url,
+
+        // footer
+        public string $address,
+        public string $unsubscribe_url,
+        public string $unsubscribe_text,
+
+        // colors in HEX format
+        public string $color_accent,
+        public string $color_background,
+        public string $color_text,
+        public string $color_box_background,
+        public string $color_box_radius,
+        public string $color_box_shadow,
+        public string $color_box_border,
     )
     {
     }
