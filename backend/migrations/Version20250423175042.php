a0e52dc91960dfb6fefc410ed52714b1934c625a
Thibault <thibault.boutet.dev@gmail.com>
2025-04-23 18:07:16 +0200

    Add migration + entity

--- backend/migrations/Version20250423175042.php
+++ backend/migrations/Version20250423175042.php
@@ -10,28 +10,31 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250318233600 extends AbstractMigration
+final class Version20250423175042 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Create templates table';
+        return 'Create domains table';
     }
 
     public function up(Schema $schema): void
     {
         $this->addSql(<<<SQL
-        CREATE TABLE templates (
+        CREATE TABLE domains (
             id BIGSERIAL PRIMARY KEY,
             created_at timestamptz DEFAULT CURRENT_TIMESTAMP NOT NULL,
             updated_at timestamptz DEFAULT CURRENT_TIMESTAMP NOT NULL,
-            project_id BIGINT NOT NULL REFERENCES projects(id),
-            template TEXT NOT NULL
+            dkim_public_key TEXT,
+            dkim_private_key TEXT,
+            user_id BIGINT NOT NULL,
+            verified_in_ses BOOLEAN DEFAULT FALSE
         );
         SQL);
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('DROP TABLE templates');
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE domains');
     }
 }
