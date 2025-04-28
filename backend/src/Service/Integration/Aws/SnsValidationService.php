bd17d7ffdeeaef3796d7c902689b1fac936f08b9
Supun Wimalasena <supun@hyvor.com>
2025-04-28 13:52:49 +0200

    mocking

--- backend/src/Service/Integration/Aws/SnsValidationService.php
+++ backend/src/Service/Integration/Aws/SnsValidationService.php
@@ -2,24 +2,11 @@
 
 namespace App\Service\Integration\Aws;
 
-use Aws\SesV2\SesV2Client;
 use Aws\Sns\Message;
 use Aws\Sns\MessageValidator;
 
 class SnsValidationService
 {
-
-    public function get_client(): SesV2Client
-    {
-        return new SesV2Client([
-            'region' => $_ENV['AWS_REGION'],
-            'version' => 'latest',
-            'credentials' => [
-                'key' => $_ENV['AWS_ACCESS_KEY_ID'],
-                'secret' => $_ENV['AWS_SECRET_ACCESS_KEY'],
-            ],
-        ]);
-    }
 
     /**
      * @param array<mixed> $data
