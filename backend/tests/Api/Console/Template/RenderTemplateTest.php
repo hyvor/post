36f6d095f7efaa93efafde956430c92669719fb2
Thibault <thibault.boutet.dev@gmail.com>
2025-05-13 08:49:16 +0200

    Add test for render template

---
+++ backend/tests/Api/Console/Template/RenderTemplateTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Api\Console\Template;
+
+use App\Tests\Case\WebTestCase;
+use App\Tests\Factory\ProjectFactory;
+
+class RenderTemplateTest extends WebTestCase
+{
+    public function test_render_template(): void
+    {
+        $project = ProjectFactory::createOne();
+
+        $response = $this->consoleApi(
+            $project,
+            'POST',
+            '/templates/render',
+            [
+                'template' => 'Test'
+            ]
+        );
+
+        $this->assertSame(200, $response->getStatusCode());
+        $json = $this->getJson($response);
+
+        $this->assertSame('Test', $json['html']);
+    }
+}
