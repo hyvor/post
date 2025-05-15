a70c7a41898be9910b400767e108ffe75555e571
Supun Wimalasena <supun@hyvor.com>
2025-05-15 12:34:43 +0200

    usage done

--- backend/src/Api/Console/Controller/BillingController.php
+++ backend/src/Api/Console/Controller/BillingController.php
@@ -3,10 +3,12 @@
 namespace App\Api\Console\Controller;
 
 use App\Service\Issue\SendService;
+use Hyvor\Internal\Billing\License\PostLicense;
 use Hyvor\Internal\Bundle\Security\HasHyvorUser;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Hyvor\Internal\Billing\BillingInterface;
 
 class BillingController extends AbstractController
 {
@@ -15,22 +17,24 @@ class BillingController extends AbstractController
 
     public function __construct(
         private SendService $sendService
-    )
-    {
+    ) {
     }
 
     #[Route('/billing/usage', methods: 'GET')]
-    public function getUsage(): JsonResponse
+    public function getUsage(BillingInterface $billing): JsonResponse
     {
         $user = $this->getHyvorUser();
 
+        /** @var ?PostLicense $license */
+        $license = $billing->license($user->id, null);
+
         return new JsonResponse([
             'emails' => [
+                'limit' => $license->emails ?? 0,
                 'this_month' => $this->sendService->getSendsCountThisMonthOfUser($user->id),
                 'last_12_months' => $this->sendService->getSendsCountLast12MonthsOfUser($user->id),
             ]
         ]);
-
     }
 
 }
