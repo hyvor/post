a32e0b8729032633b24934545c57274af81fdbe3
Thibault <thibault.boutet.dev@gmail.com>
2025-05-22 16:54:25 +0200

    Add export function

---
+++ backend/src/Service/Subscriber/SubscriberCsvExporter.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Service\Subscriber;
+
+use App\Entity\Project;
+use App\Entity\Subscriber;
+use App\Entity\SubscriberMetadataDefinition;
+use Doctrine\ORM\EntityManagerInterface;
+
+class SubscriberCsvExporter
+{
+    public function __construct(
+        private EntityManagerInterface $em,
+    ) {
+    }
+
+    protected function getTemporaryFile(string $extension): string
+    {
+        $file = tmpfile();
+
+        if (!$file) {
+            throw new \Exception('Could not create temporary file.');
+        }
+
+        $meta = stream_get_meta_data($file);
+        $path = $meta['uri'];
+        fclose($file);
+        return $path . '.' . $extension;
+    }
+
+    public function createFile(Project $project): string
+    {
+        $csv = $this->getTemporaryFile('csv');
+
+        $file = fopen($csv, 'w');
+
+        if (!$file) {
+            throw new \Exception('Could not create file');
+        }
+
+        $subscriberMetadata = $this->em->getRepository(SubscriberMetadataDefinition::class)->findBy(['project' => $project]);
+        $headers = ['Email', 'Status', 'Subscribed At', 'Source'];
+        foreach ($subscriberMetadata as $sb) {
+            $headers[] = $sb->getKey();
+        }
+
+        fputcsv($file, $headers);
+
+        $batchSize = 1000;
+        $offset = 0;
+
+        do {
+            /** @var Subscriber[] $subscribers */
+            $subscribers = $this->em->getRepository(Subscriber::class)
+                ->createQueryBuilder('s')
+                ->where('s.project = :project')
+                ->setParameter('project', $project)
+                ->orderBy('s.id', 'ASC')
+                ->setFirstResult($offset)
+                ->setMaxResults($batchSize)
+                ->getQuery()
+                ->getResult();
+
+            foreach ($subscribers as $subscriber) {
+                fputcsv($file, [
+                    $subscriber->getEmail(),
+                    $subscriber->getStatus()->value,
+                    $subscriber->getSubscribedAt()?->format('Y-m-d H:i:s') ?? '',
+                    $subscriber->getSource()->value,
+                ]);
+            }
+
+            $offset += $batchSize;
+        } while ($subscribers && count($subscribers) === $batchSize);
+
+        return $csv;
+    }
+}
