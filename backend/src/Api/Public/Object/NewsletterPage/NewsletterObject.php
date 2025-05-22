24b16715d8dfbf82d536540fb36a7ea56aca48a8
Supun Wimalasena <44988673+supun-io@users.noreply.github.com>
2025-05-22 18:10:56 +0200

    Newsletter Page Issue (#109)
    * templates wip
    
    * newsletter issue page

--- backend/src/Api/Public/Object/NewsletterPage/NewsletterObject.php
+++ backend/src/Api/Public/Object/NewsletterPage/NewsletterObject.php
@@ -8,12 +8,14 @@ class NewsletterObject
 {
 
     public string $uuid;
+    public string $slug;
     public string $name;
     public ?string $logo;
 
     public function __construct(Newsletter $newsletter)
     {
         $this->uuid = $newsletter->getUuid();
+        $this->slug = $newsletter->getSlug();
         $this->name = $newsletter->getName();
 
         $meta = $newsletter->getMeta();
--- frontend/src/routes/newsletter/[slug]/newsletterPageTypes.ts
+++ frontend/src/routes/newsletter/[slug]/newsletterPageTypes.ts
@@ -1,9 +1,17 @@
 export interface Newsletter {
     uuid: string;
+    slug: string;
     name: string;
     logo: string | null;
 }
 
+export interface IssueList {
+    uuid: string;
+    subject: string;
+    sent_at: number;
+}
+
 export interface Issue {
-    
+    subject: string;
+    html: string;
 }
