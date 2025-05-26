3ebca82e54a88ec13589c341f5065c726d319c2f
Supun Wimalasena <44988673+supun-io@users.noreply.github.com>
2025-05-26 13:16:35 +0200

    Staging deployment (#120)
    * draft wip
    
    * upto lists done
    
    * from email wip
    
    * billing wip
    
    * home refactor, stats updated backend
    
    * stats completed
    
    * lists wip

--- frontend/src/routes/console/[id]/home/ListEditionModal.svelte
+++ frontend/src/routes/console/[id]/home/ListEditionModal.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
-	import { FormControl, Label, Modal, SplitControl, TextInput } from "@hyvor/design/components";
-	import { onMount } from "svelte";
+	import { FormControl, Modal, SplitControl, TextInput } from '@hyvor/design/components';
 
-    export let modalOpen = false;
-    export let listName: string = '';
-    export let listDescription: string|null = null;
+	export let modalOpen = false;
+	export let listName: string = '';
+	export let listDescription: string | null = null;
 
-    export let submitModal: () => void;
+	export let submitModal: () => void;
 
 	let nameInput: HTMLInputElement | null = null;
 
@@ -16,54 +15,41 @@
 </script>
 
 <Modal
-	title={listName === '' ? "Create List" : "Edit List"}
+	title={listName === '' ? 'Create List' : 'Edit List'}
 	bind:show={modalOpen}
 	size="medium"
 	footer={{
-        cancel: {
-            text: 'Cancel',
-        }, 
-        confirm: {
-            text: listName === '' ? 'Create' : 'Save',
-        }
-    }}
-    on:cancel={() => {
+		cancel: {
+			text: 'Cancel'
+		},
+		confirm: {
+			text: listName === '' ? 'Create' : 'Save'
+		}
+	}}
+	on:cancel={() => {
 		modalOpen = false;
 	}}
-    on:confirm={() => {
-        modalOpen = false;
+	on:confirm={() => {
+		modalOpen = false;
 		submitModal();
-    }}
+	}}
 >
-
 	<div class="modal-inner">
 		<FormControl>
-			<SplitControl 
-				label="Name"
-				caption="The name of the list."
-			>
-				<TextInput 
+			<SplitControl label="Name" caption="The name of the list.">
+				<TextInput
 					maxlength={255}
-					placeholder="Enter list name" 
+					placeholder="Enter list name"
 					bind:value={listName}
 					bind:input={nameInput!}
 				/>
 			</SplitControl>
-			<SplitControl 
-				label="Description"
-				caption="The description of the list."
-			>
-				<TextInput 
-					placeholder="Enter list description" 
-					bind:value={listDescription}
-				/>
+			<SplitControl label="Description" caption="The description of the list.">
+				<TextInput placeholder="Enter list description" bind:value={listDescription} />
 			</SplitControl>
 		</FormControl>
-
 	</div>
-
 </Modal>
 
 <style>
-
 </style>
--- frontend/src/routes/console/[id]/home/ListView.svelte
+++ frontend/src/routes/console/[id]/home/ListView.svelte
@@ -2,15 +2,21 @@
 	import ListRow from './ListRow.svelte';
 	import CreateListButton from './CreateListButton.svelte';
 	import { listStore } from '../../lib/stores/newsletterStore';
+	import { getI18n } from '../../lib/i18n';
+	import IconInfoCircle from '@hyvor/icons/IconInfoCircle';
+	import { Tooltip } from '@hyvor/design/components';
 
-	$: {
-		$listStore;
-	}
+	const I18n = getI18n();
 </script>
 
 <div id="list-view">
 	<div class="list-header">
-		Lists
+		<Tooltip text={I18n.t('console.home.lists.tooltip')}>
+			<div class="left">
+				{I18n.t('console.home.lists.title')}
+				<IconInfoCircle />
+			</div>
+		</Tooltip>
 		<CreateListButton />
 	</div>
 	<div class="rows">
@@ -29,6 +35,12 @@
 		padding-left: 35px;
 		padding-right: 35px;
 		font-weight: 600;
+	}
+	.left {
+		display: flex;
+		align-items: center;
+		gap: 5px;
+		cursor: pointer;
 	}
 	.rows {
 		padding: 10px 35px;
