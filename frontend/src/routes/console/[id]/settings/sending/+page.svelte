70241c1d405ad6a01112817073a3cc38e53fa6db
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2025-07-09 00:25:07 +0530

    remove sending addresses

--- frontend/src/routes/console/[id]/settings/sending/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-import SettingsBody from '../@components/SettingsBody.svelte';
-	import SendingAddresses from './SendingAddresses.svelte';
-</script>
-
-<SettingsBody>
-    <SendingAddresses />
-</SettingsBody>
