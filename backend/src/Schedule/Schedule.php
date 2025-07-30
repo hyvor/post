16cc80348e36959e7e6254ff2b7015145f38fbb7
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2025-07-30 12:12:44 +0530

    lock

--- backend/src/Schedule/Schedule.php
+++ backend/src/Schedule/Schedule.php
@@ -3,6 +3,7 @@
 namespace App\Schedule;
 
 use App\Service\Subscriber\Message\ClearPendingSubscribersMessage;
+use Symfony\Component\Lock\LockFactory;
 use Symfony\Component\Scheduler\Attribute\AsSchedule;
 use Symfony\Component\Scheduler\RecurringMessage;
 use Symfony\Component\Scheduler\Schedule as SymfonySchedule;
@@ -11,12 +12,15 @@ use Symfony\Component\Scheduler\ScheduleProviderInterface;
 #[AsSchedule(name: 'global')]
 class Schedule implements ScheduleProviderInterface
 {
-    public function __construct() {
+    public function __construct(
+        private LockFactory $lockFactory
+    ) {
     }
 
     public function getSchedule(): SymfonySchedule
     {
         return new SymfonySchedule()
-            ->add(RecurringMessage::every('1 day', new ClearPendingSubscribersMessage));
+            ->add(RecurringMessage::every('1 day', new ClearPendingSubscribersMessage))
+            ->lock($this->lockFactory->createLock('clear_pending_subscribers'));
     }
 }
