0c3125bb16ac10acf0852a471bb84aba38e638e5
Nadil Karunarathna <113877141+Nadil-K@users.noreply.github.com>
2025-07-30 19:00:24 +0530

    Delete pending subscribers (#160)
    * temp fix
    
    * clear pending subscriber job
    
    * update test coverage
    
    * lock
    
    * wip
    
    ---------
    
    Co-authored-by: Supun Wimalasena <supun@hyvor.com>

---
+++ backend/src/Schedule/Schedule.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Schedule;
+
+use App\Service\Subscriber\Message\ClearPendingSubscribersMessage;
+use Symfony\Component\Lock\LockFactory;
+use Symfony\Component\Scheduler\Attribute\AsSchedule;
+use Symfony\Component\Scheduler\RecurringMessage;
+use Symfony\Component\Scheduler\Schedule as SymfonySchedule;
+use Symfony\Component\Scheduler\ScheduleProviderInterface;
+
+#[AsSchedule()]
+class Schedule implements ScheduleProviderInterface
+{
+    public function __construct(
+        private LockFactory $lockFactory
+    ) {
+    }
+
+    public function getSchedule(): SymfonySchedule
+    {
+        return new SymfonySchedule()
+            ->add(RecurringMessage::every('1 day', new ClearPendingSubscribersMessage))
+            ->lock($this->lockFactory->createLock('schedule'));
+    }
+}
