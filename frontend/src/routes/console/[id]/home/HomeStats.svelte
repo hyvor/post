e1251fa24b59445fa57fcee0850095dcb1867af8
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2025-08-19 16:40:10 +0530

    frontend

--- frontend/src/routes/console/[id]/home/HomeStats.svelte
+++ frontend/src/routes/console/[id]/home/HomeStats.svelte
@@ -1,51 +1,52 @@
 <script lang="ts">
-	import { Loader } from '@hyvor/design/components';
-	import Stat from './Stat.svelte';
-	import { newsletterStatsStore, newsletterStore } from '../../lib/stores/newsletterStore';
-	import { getI18n } from '../../lib/i18n';
+    import {Loader} from '@hyvor/design/components';
+    import Stat from './Stat.svelte';
+    import {newsletterStatsStore, newsletterStore} from '../../lib/stores/newsletterStore';
+    import {getI18n} from '../../lib/i18n';
 
-	const I18n = getI18n();
+    const I18n = getI18n();
 
-	let loading = false;
+    let loading = false;
 </script>
 
 <div class="wrap">
-	{#if loading}
-		<Loader block padding={25} />
-	{:else}
-		<div class="stats">
-			<Stat
-				title={I18n.t('console.nav.subscribers')}
-				counts={$newsletterStatsStore.subscribers}
-				href={`/console/${$newsletterStore.id}/subscribers`}
-			/>
-			<Stat
-				title={I18n.t('console.nav.issues')}
-				counts={$newsletterStatsStore.issues}
-				href={`/console/${$newsletterStore.id}/issues`}
-			/>
-			<Stat
-				title={I18n.t('console.home.stats.openRate')}
-				counts={$newsletterStatsStore.open_rate}
-				percent
-			/>
-			<Stat
-				title={I18n.t('console.home.stats.clickRate')}
-				counts={$newsletterStatsStore.click_rate}
-				percent
-			/>
-		</div>
-	{/if}
+    {#if loading}
+        <Loader block padding={25}/>
+    {:else}
+        <div class="stats">
+            <Stat
+                title={I18n.t('console.nav.subscribers')}
+                counts={$newsletterStatsStore.subscribers}
+                href={`/console/${$newsletterStore.id}/subscribers`}
+            />
+            <Stat
+                title={I18n.t('console.nav.issues')}
+                counts={$newsletterStatsStore.issues}
+                href={`/console/${$newsletterStore.id}/issues`}
+            />
+            <Stat
+                title={I18n.t('console.home.stats.bouncedRate')}
+                counts={$newsletterStatsStore.bounced_rate}
+                percent
+            />
+            <Stat
+                title={I18n.t('console.home.stats.complainedRate')}
+                counts={$newsletterStatsStore.complained_rate}
+                percent
+            />
+        </div>
+    {/if}
 </div>
 
 <style>
-	.stats {
-		display: grid;
-		grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-		gap: 15px;
-	}
-	.wrap {
-		padding: 25px 35px;
-		border-bottom: 1px solid var(--border);
-	}
+    .stats {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+        gap: 15px;
+    }
+
+    .wrap {
+        padding: 25px 35px;
+        border-bottom: 1px solid var(--border);
+    }
 </style>
