5c7ffe2507a4f9fc0d9189c94b856d8f65a600e8
Supun Wimalasena <44988673+supun-io@users.noreply.github.com>
2025-09-22 16:15:26 +0530

    Rich Text Editor (#194)
    * clean up old editor
    
    * add rt editor
    
    * colors

--- frontend/src/routes/console/[id]/issues/Editor/Editor.svelte
+++ frontend/src/routes/console/[id]/issues/Editor/Editor.svelte
@@ -1,13 +1,27 @@
 <script lang="ts">
-    import {Editor} from '@hyvor/richtext';
-    import type {EditorView} from 'prosemirror-view';
+	import { Editor } from '@hyvor/richtext';
 
-    interface Props {
-        content?: string | null;
-        onDocUpdate: (doc: string) => void;
-    }
+	interface Props {
+		content?: string | null;
+		onDocUpdate: (doc: string) => void;
+	}
 
-    let {content = null, onDocUpdate}: Props = $props();
+	let { content = null, onDocUpdate }: Props = $props();
 </script>
 
-<Editor value={content} onvaluechange={onDocUpdate}/>
+<Editor
+	value={content}
+	onvaluechange={onDocUpdate}
+	config={{
+		// colorButtonText:
+		// colorButtonBackground:
+
+		tableEnabled: false, // add later
+		tocEnabled: false,
+		bookmarkEnabled: false, // add later
+
+		// TODO:
+		imageUploader: async () => null,
+		audioUploader: async () => null
+	}}
+/>
