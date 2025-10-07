536b1d63012c70c22b417e5f2c8f9f629157b587
Supun Wimalasena <supun@hyvor.com>
2025-10-07 23:36:10 +0200

    load ENV on server side

--- archive/src/lib/actions/subscriptionActions.ts
+++ archive/src/lib/actions/subscriptionActions.ts
@@ -1,32 +1,32 @@
-import publicApi from "$lib/publicApi";
-import type {List} from "$lib/types";
+import publicApi from '$lib/publicApi.svelte';
+import type { List } from '$lib/types';
 
 const SUBSCRIBER_PREFIX = '/subscriber';
 
 export function confirm(token: string) {
-    return publicApi.get<void>({
-        endpoint: SUBSCRIBER_PREFIX + '/confirm',
-        data: {token},
-    });
+	return publicApi.get<void>({
+		endpoint: SUBSCRIBER_PREFIX + '/confirm',
+		data: { token }
+	});
 }
 
 interface UnsubscribeResponse {
-    lists: List[]
+	lists: List[];
 }
 
 export function unsubscribe(token: string) {
-    return publicApi.get<UnsubscribeResponse>({
-        endpoint: SUBSCRIBER_PREFIX + '/unsubscribe',
-        data: {token},
-    });
+	return publicApi.get<UnsubscribeResponse>({
+		endpoint: SUBSCRIBER_PREFIX + '/unsubscribe',
+		data: { token }
+	});
 }
 
 export function resubscribe(list_ids: number[], token: string) {
-    return publicApi.patch<void>({
-        endpoint: SUBSCRIBER_PREFIX + '/resubscribe',
-        data: {
-            list_ids,
-            token
-        },
-    })
+	return publicApi.patch<void>({
+		endpoint: SUBSCRIBER_PREFIX + '/resubscribe',
+		data: {
+			list_ids,
+			token
+		}
+	});
 }
