4afd87cc8c756cc53d874868c5e348bb13c3a135
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2025-11-03 17:05:36 +0530

    approval button disable

--- frontend/src/routes/sudo/approvals/ApprovalModal.svelte
+++ frontend/src/routes/sudo/approvals/ApprovalModal.svelte
@@ -113,13 +113,13 @@
 
     {#snippet footer() }
         <div class="footer-buttons">
-            <Button color="input" on:click={() => onMarkAsPending(approval)}>
+            <Button color="input" disabled={approval.status === 'pending'} on:click={() => onMarkAsPending(approval)}>
                 Mark as pending
             </Button>
-            <Button color="red" on:click={ () => onReject(approval) }>
+            <Button color="red" disabled={approval.status === 'rejected'} on:click={ () => onReject(approval) }>
                 Reject
             </Button>
-            <Button on:click={ () => onApprove(approval) }>
+            <Button disabled={approval.status === 'approved'} on:click={ () => onApprove(approval) }>
                 Approve
             </Button>
         </div>
