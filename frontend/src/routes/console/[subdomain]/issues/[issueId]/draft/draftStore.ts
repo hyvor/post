d6fc072d05cd62da3f775c3176e0262dd192c7c7
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2025-11-11 17:44:07 +0530

    draftStep

--- frontend/src/routes/console/[subdomain]/issues/[issueId]/draft/draftStore.ts
+++ frontend/src/routes/console/[subdomain]/issues/[issueId]/draft/draftStore.ts
@@ -1,5 +1,5 @@
-import { writable } from "svelte/store";
-import { type Issue } from "../../../../types";
+import {writable} from "svelte/store";
+import {type Issue} from "../../../../types";
 
 export const draftIssueStore = writable<Issue>({} as Issue);
 export const draftIssueEditingStore = writable<Issue>({} as Issue);
@@ -13,8 +13,21 @@ export const draftSendableSubscribersCountStore = writable({
 
 export function initDraftStores(issue: Issue) {
     draftIssueStore.set(issue);
-    draftIssueEditingStore.set({ ...issue });
+    draftIssueEditingStore.set({...issue});
 }
 
 export type StepKey = 'content' | 'audience';
-export const draftStepStore = writable<StepKey>('content');
+export const draftStepStore = writable<Record<number, StepKey>>({});
+
+draftIssueStore.subscribe(issue => {
+
+    const issueId = issue?.id;
+    if (!issueId) return;
+
+    draftStepStore.update(steps => {
+        if (steps[issueId] === undefined) {
+            steps[issueId] = 'content';
+        }
+        return steps;
+    });
+});
