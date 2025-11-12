8e90779ff0ad54f76cf81c513ce2e95063bcb2c1
Supun Wimalasena <supun@hyvor.com>
2025-11-12 17:03:24 +0100

    cleanup

--- frontend/src/routes/console/(nav)/[subdomain]/+layout.svelte
+++ frontend/src/routes/console/(nav)/[subdomain]/+layout.svelte
@@ -1,99 +1,86 @@
 <script lang="ts">
-    import Nav from '../../@components/Nav/Nav.svelte';
-    import {page} from '$app/state';
-    import {loadNewsletter} from '../../lib/newsletterLoader';
-    import {Loader, toast} from '@hyvor/design/components';
-    import NewsletterSelector from '../../@components/Nav/NewsletterSelector.svelte';
-    import {userNewslettersStore} from '../../lib/stores/userNewslettersStore';
-    import {newsletterStore} from '../../lib/stores/newsletterStore';
+	import Nav from '../../@components/Nav/Nav.svelte';
+	import { page } from '$app/state';
+	import { loadNewsletter } from '../../lib/newsletterLoader';
+	import { Loader, toast } from '@hyvor/design/components';
+	import NewsletterSelector from '../../@components/Nav/NewsletterSelector.svelte';
+	import { userNewslettersStore } from '../../lib/stores/userNewslettersStore';
+	import { newsletterStore } from '../../lib/stores/newsletterStore';
 
-    interface Props {
-        children?: import('svelte').Snippet;
-    }
+	interface Props {
+		children?: import('svelte').Snippet;
+	}
 
-    let {children}: Props = $props();
+	let { children }: Props = $props();
 
-    let subdomain = $derived(String(page.params.subdomain))
-    let isLoading = $state(true);
+	let subdomain = $derived(String(page.params.subdomain));
+	let isLoading = $state(true);
 
-    function load() {
-        const userNewsletter = $userNewslettersStore.find(
-            (n) => n.newsletter.subdomain === subdomain
-        );
+	function load() {
+		const userNewsletter = $userNewslettersStore.find(
+			(n) => n.newsletter.subdomain === subdomain
+		);
 
-        if (!userNewsletter) {
-            toast.error('Newsletter not found');
-            location.href = '/console';
-            return;
-        }
+		if (!userNewsletter) {
+			toast.error('Newsletter not found');
+			location.href = '/console';
+			return;
+		}
 
-        loadNewsletter(userNewsletter.newsletter.id)
-            .then(() => {
-                isLoading = false;
-            })
-            .catch((e) => {
-                toast.error('Unable to load newsletter:' + e.message);
-            });
-    }
+		loadNewsletter(userNewsletter.newsletter.id)
+			.then(() => {
+				isLoading = false;
+			})
+			.catch((e) => {
+				toast.error('Unable to load newsletter:' + e.message);
+			});
+	}
 
-    $effect(() => {
-        subdomain;
-        load();
-    });
+	$effect(() => {
+		subdomain;
+		load();
+	});
 </script>
 
-<div class="main-inner">
-    {#if isLoading}
-        <div class="full-loader">
-            <Loader size="large"/>
-        </div>
-    {:else}
-        <Nav/>
-        <NewsletterSelector/>
-        {#key $newsletterStore.subdomain}
-            <div class="content">
-                {@render children?.()}
-            </div>
-        {/key}
-    {/if}
-</div>
+{#if isLoading}
+	<div class="full-loader">
+		<Loader size="large" />
+	</div>
+{:else}
+	{#key $newsletterStore.subdomain}
+		<div class="content">
+			{@render children?.()}
+		</div>
+	{/key}
+{/if}
 
 <style>
-    .main-inner {
-        display: flex;
-        flex: 1;
-        width: 100%;
-        height: 100%;
-        min-height: 0;
-    }
+	.content {
+		display: flex;
+		flex-direction: column;
+		flex: 1;
+		width: 100%;
+		height: 100%;
+		min-width: 0;
+	}
 
-    .content {
-        display: flex;
-        flex-direction: column;
-        padding: 15px;
-        flex: 1;
-        width: 100%;
-        height: 100%;
-        min-width: 0;
-    }
+	.full-loader {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
 
-    .full-loader {
-        width: 100%;
-        height: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+	@media (max-width: 992px) {
+		.main-inner {
+			display: block;
+		}
 
-    @media (max-width: 992px) {
-        .main-inner {
-            display: block;
-        }
-
-        .content {
-            padding-bottom: 150px;
-            height: initial;
-            min-height: calc(100vh - 50px);
-        }
-    }
+		.content {
+			padding-bottom: 150px;
+			height: initial;
+			min-height: calc(100vh - 50px);
+		}
+	}
 </style>
