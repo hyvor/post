605f3380905c9ebfb704de763f7188319a951a09
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2025-11-17 17:42:43 +0530

    focus input elements

--- frontend/src/routes/console/(nav)/[subdomain]/issues/[issueId]/draft/content/Content.svelte
+++ frontend/src/routes/console/(nav)/[subdomain]/issues/[issueId]/draft/content/Content.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-    import { Editor } from '@hyvor/richtext';
+    import {Editor} from '@hyvor/richtext';
     import {draftIssueEditingStore} from '../draftStore';
     import {debouncedUpdateDraftIssue} from '../draftActions';
     import {getI18n} from '../../../../../../lib/i18n';
-	import { newsletterStore } from '../../../../../../lib/stores/newsletterStore';
-	import { uploadImage } from '../../../../../../lib/actions/mediaActions';
+    import {newsletterStore} from '../../../../../../lib/stores/newsletterStore';
+    import {uploadImage} from '../../../../../../lib/actions/mediaActions';
+    import {onMount} from "svelte";
 
     function onContentDocUpdate(doc: string) {
         $draftIssueEditingStore.content = doc;
@@ -25,14 +26,14 @@
     }
 
     function handleDomEvent(name: string, e: Event) {
-        
+
         // focus the subject input back
         if (name === 'keydown' && ['ArrowUp', 'Backspace'].includes((e as KeyboardEvent).key)) {
             const selection = editorView.state.selection;
             if (selection.from === 1 && selection.to === 1) {
                 e.preventDefault();
                 subjectInput.focus();
-            }   
+            }
         }
 
     }
@@ -41,29 +42,33 @@
         focus(): void;
     }
 
-    let editorView: EditorView&any = $state({} as any);
+    let editorView: EditorView & any = $state({} as any);
     let subjectInput: HTMLInputElement = $state({} as HTMLInputElement);
 
     const I18n = getI18n();
+
+    onMount(() => {
+        subjectInput.focus();
+    });
 </script>
 
 <div class="content-wrap">
     <div class="content-inner">
         <input
-            type="text"
-            placeholder={I18n.t('console.issues.draft.subjectPlaceholder')}
-            value={$draftIssueEditingStore.subject}
-            onchange={onSubjectUpdate}
-            onkeydown={handleKeyDown}
-            bind:this={subjectInput}
+                type="text"
+                placeholder={I18n.t('console.issues.draft.subjectPlaceholder')}
+                value={$draftIssueEditingStore.subject}
+                onchange={onSubjectUpdate}
+                onkeydown={handleKeyDown}
+                bind:this={subjectInput}
         />
 
         <Editor
-            bind:editorView={editorView}
-            value={$draftIssueEditingStore.content}
-            onvaluechange={onContentDocUpdate}
-            ondomevent={handleDomEvent}
-            config={{
+                bind:editorView={editorView}
+                value={$draftIssueEditingStore.content}
+                onvaluechange={onContentDocUpdate}
+                ondomevent={handleDomEvent}
+                config={{
                 colorButtonBackground: $newsletterStore?.template_color_accent || '#5A8387',
                 colorButtonText: $newsletterStore?.template_color_accent_text || '#ffffff',
 
