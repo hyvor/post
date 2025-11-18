85744b74504b768a301c0c285314504b38e29c9c
Nadil Karunarathna <nadilkarunaratna2000@gmail.com>
2025-11-18 14:21:47 +0530

    php stan fixes

--- backend/src/Api/Public/Object/Archive/NewsletterObject.php
+++ backend/src/Api/Public/Object/Archive/NewsletterObject.php
@@ -9,15 +9,10 @@ class NewsletterObject
 
     public string $subdomain;
     public string $name;
-    public ?string $logo;
 
     public function __construct(Newsletter $newsletter)
     {
         $this->subdomain = $newsletter->getSubdomain();
         $this->name = $newsletter->getName();
-
-        $meta = $newsletter->getMeta();
-        $this->logo = $meta->logo;
     }
-
 }
