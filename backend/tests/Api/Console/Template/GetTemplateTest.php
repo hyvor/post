c4c6a82ab6657640881b2d1d8d9557fcd2ffe42e
Supun Wimalasena <supun@hyvor.com>
2025-11-20 05:52:02 +0100

    query param rename

--- backend/tests/Api/Console/Template/GetTemplateTest.php
+++ backend/tests/Api/Console/Template/GetTemplateTest.php
@@ -296,7 +296,7 @@ class GetTemplateTest extends WebTestCase
 {% if branding %}
     <div style="text-align:center;">
         <a class="branding-tag" target="_blank"
-           href="https://post.hyvor.com?source=mail-branding&source-subdomain={{ subdomain }}">
+           href="https://post.hyvor.com?source=mail-branding&newsletter={{ subdomain }}">
             [<span style="vertical-align:middle">Sent privately via</span> <strong style="vertical-align:middle">Hyvor
                 Post</strong> <img style="vertical-align:middle" src="https://post.hyvor.com/img/logo.png" width="14"/>]
         </a>
