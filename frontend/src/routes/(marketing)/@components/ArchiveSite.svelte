650a2d7e6711bd4cceec7ba270cba5d7f199e9d3
IshiniAvindya <ishiniavindya2000@gmail.com>
2025-12-01 11:51:08 +0100

    Update ArchiveSite.svelte

--- frontend/src/routes/(marketing)/@components/ArchiveSite.svelte
+++ frontend/src/routes/(marketing)/@components/ArchiveSite.svelte
@@ -244,20 +244,6 @@
 		.seo-feature-description {
 			word-break: break-word;
 		}
-		/* .bento-grid {
-			grid-template-columns: 1fr;
-			gap: 20px;
-			width: 100%;
-			justify-content: center; 
-			flex-wrap: wrap; 
-			width: 100%;
-		}
-
-		.int {
-			justify-content: center;
-			width: 100%;
-			max-width: 420px;
-		} */
 	}
 
 	@media (max-width: 767px) {
