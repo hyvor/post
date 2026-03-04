6fe3e8a1a41852a092307619f0ef1396c2951f00
Supun Wimalasena <supun@hyvor.com>
2026-03-04 11:52:17 +0100

    sending confirmation mail

--- backend/src/Service/Subscriber/ConfirmationMail/SendConfirmationMailMessage.php
+++ backend/src/Service/Subscriber/ConfirmationMail/SendConfirmationMailMessage.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace App\Service\Subscriber\Message;
+namespace App\Service\Subscriber\ConfirmationMail;
 
 use App\Service\App\Messenger\MessageTransport;
 use Symfony\Component\Messenger\Attribute\AsMessage;
 
 #[AsMessage(MessageTransport::ASYNC)]
-readonly class SendConfirmationEmailMessage
+readonly class SendConfirmationMailMessage
 {
     public function __construct(
         private int $subscriberId,
