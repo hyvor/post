3142da884763b6540450c39072d57c9ba0de1da4
Ishini Senanayake <ishiniavindya2000@gmail.com>
2026-03-23 10:18:18 +0530

    prettier run
    #419

--- frontend/src/routes/(marketing)/docs/[...slug]/docs.ts
+++ frontend/src/routes/(marketing)/docs/[...slug]/docs.ts
@@ -1,82 +1,85 @@
-import EmailDesign from './content/EmailDesign.svelte';
-import Introduction from './content/Introduction.svelte';
-import type { Component } from 'svelte';
-import SignupForm from './content/SignupForm.svelte';
-import Import from './content/Import/Import.svelte';
-import Approval from './content/Approval.svelte';
-import ConsoleApi from './content/ConsoleApi.svelte';
-import InstallDoc from './content/InstallDoc.svelte';
+import EmailDesign from "./content/EmailDesign.svelte";
+import Introduction from "./content/Introduction.svelte";
+import type { Component } from "svelte";
+import SignupForm from "./content/SignupForm.svelte";
+import Import from "./content/Import/Import.svelte";
+import Approval from "./content/Approval.svelte";
+import ConsoleApi from "./content/ConsoleApi.svelte";
+import InstallDoc from "./content/InstallDoc.svelte";
 
 export const categories: Category[] = [
-	{
-		name: 'Intro',
-		pages: [
-			{
-				slug: '',
-				name: 'Introduction',
-				component: Introduction
-			},
-			{
-				slug: 'approval',
-				name: 'Approval',
-				component: Approval
-			},
+  {
+    name: "Intro",
+    pages: [
+      {
+        slug: "",
+        name: "Introduction",
+        component: Introduction,
+      },
+      {
+        slug: "approval",
+        name: "Approval",
+        component: Approval,
+      },
 
-			{
-				slug: 'install',
-				name: 'Install',
-				component: InstallDoc
-			}
-		]
-	},
+      {
+        slug: "install",
+        name: "Install",
+        component: InstallDoc,
+      },
+    ],
+  },
 
-	{
-		name: 'Features',
-		pages: [
-			{
-				slug: 'form',
-				name: 'Signup Form',
-				component: SignupForm
-			},
-			{
-				slug: 'design',
-				name: 'Email Design',
-				component: EmailDesign
-			},
-			{
-				slug: 'import',
-				name: 'Import',
-				component: Import
-			}
-		]
-	},
-	{
-		name: 'Developer',
-		pages: [
-			// {
-			// 	slug: 'webhooks',
-			// 	name: 'Webhooks'
-			// 	// component: add component name
-			// },
-			{
-				slug: 'api-console',
-				name: 'Console API',
-				component: ConsoleApi
-			}
-		]
-	}
+  {
+    name: "Features",
+    pages: [
+      {
+        slug: "form",
+        name: "Signup Form",
+        component: SignupForm,
+      },
+      {
+        slug: "design",
+        name: "Email Design",
+        component: EmailDesign,
+      },
+      {
+        slug: "import",
+        name: "Import",
+        component: Import,
+      },
+    ],
+  },
+  {
+    name: "Developer",
+    pages: [
+      // {
+      // 	slug: 'webhooks',
+      // 	name: 'Webhooks'
+      // 	// component: add component name
+      // },
+      {
+        slug: "api-console",
+        name: "Console API",
+        component: ConsoleApi,
+      },
+    ],
+  },
 ];
 
-export const pages = categories.reduce((acc, category) => acc.concat(category.pages), [] as Page[]);
+export const pages = categories.reduce(
+  (acc, category) => acc.concat(category.pages),
+  [] as Page[],
+);
 
 interface Category {
-	name: string;
-	pages: Page[];
+  name: string;
+  pages: Page[];
 }
 
 interface Page {
-	slug: string;
-	name: string;
-	component?: Component<any>;
-	parent?: string;
+  slug: string;
+  name: string;
+  component?: Component<any>;
+  parent?: string;
 }
