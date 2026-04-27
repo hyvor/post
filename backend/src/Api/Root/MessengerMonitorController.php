60941b1b7111d92b9ab02fcd27b181adcdcad218
SupunKavinda <supunkavinda1125@gmail.com>
2026-04-27 21:15:02 +0530

    monitor sudo permissions

--- backend/src/Api/Root/MessengerMonitorController.php
+++ backend/src/Api/Root/MessengerMonitorController.php
@@ -2,10 +2,11 @@
 
 namespace App\Api\Root;
 
+use App\Service\Sudo\SudoPermission;
+use Hyvor\Internal\Bundle\Api\SudoPermissionRequired;
 use Symfony\Component\Routing\Attribute\Route;
 use Zenstruck\Messenger\Monitor\Controller\MessengerMonitorController as BaseMessengerMonitorController;
 
 #[Route('/messenger')]
-class MessengerMonitorController extends BaseMessengerMonitorController
-{
-}
+#[SudoPermissionRequired(SudoPermission::ACCESS_SUDO)]
+class MessengerMonitorController extends BaseMessengerMonitorController {}
