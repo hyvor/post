931387d395684358b886e3d2b092822fcfea7527
Supun Wimalasena <supun@hyvor.com>
2026-07-15 00:35:24 +0200

    remove Crisp

--- frontend/src/routes/(marketing)/+page.svelte
+++ frontend/src/routes/(marketing)/+page.svelte
@@ -16,18 +16,6 @@
 		content="Hyvor Post is a simple, privacy-first newsletter platform. No Tracking. Just Ideas, Delivered."
 	/>
 	<link rel="canonical" href="https://post.hyvor.com" />
-
-	<script type="text/javascript">
-		window.$crisp = [];
-		window.CRISP_WEBSITE_ID = '1cab78fb-4baf-497e-a10f-00a3b12cfcfe';
-		(function () {
-			d = document;
-			s = d.createElement('script');
-			s.src = 'https://client.crisp.chat/l.js';
-			s.async = 1;
-			d.getElementsByTagName('head')[0].appendChild(s);
-		})();
-	</script>
 </svelte:head>
 
 <AboveFold />
